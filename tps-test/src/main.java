ef2e5f3f37987250ab700537cbb6ae9721b902ea
wanglei <seilwang@163.com>
2017-09-22 08:45:17 +0800

    modify tps test add client num

--- tps-test/src/main.java
+++ tps-test/src/main.java
@@ -7,9 +7,10 @@ public class main {
 	public static void main(String[] args) {
 		String IP = "localhost";
 		String PASSWORD = "";
+		int THREADNUM = 200;
 		if(args.length!=0)
 		{
-			Getopt testOpt  = new Getopt(args[0], args, "h:p:");  
+			Getopt testOpt  = new Getopt(args[0], args, "h:p:c:");  
 			int res;  
 	        while( (res = testOpt.getopt()) != -1 ) {  
 	          switch(res) {  
@@ -19,14 +20,23 @@ public class main {
 	            case 'p':  
 	            	PASSWORD = testOpt.getOptarg();  
 	              break;  
+	            case 'c':  
+	            	THREADNUM = Integer.parseInt(testOpt.getOptarg());  
+	              break; 
 	            default:  
 	              System.out.println("input ip and password!");  
 	          }  
 	        }  
 		}
 		// TODO Auto-generated method stub
-		int threadNum = 200;
+		int threadNum = THREADNUM;
 		int TPPTHREAD = 10000;
+		System.out.println("==========================================================");
+		System.out.println("Work thread num:"+threadNum+"--default 200");
+		System.out.println("Select per thread is:"+TPPTHREAD);
+		System.out.println("Total Select ops:"+TPPTHREAD*threadNum);
+		System.out.println("Running......");
+		System.out.println("==========================================================");
 		long runSqlTotalTime = 0;
 		RunSQL[] thread_sql=new RunSQL[threadNum];
 		long startTime =System.currentTimeMillis(); 
@@ -51,7 +61,6 @@ public class main {
     	}
 		long endTime =System.currentTimeMillis();
 		runSqlTotalTime = endTime - startTime;
-		System.out.println("Total TP: "+(threadNum*TPPTHREAD));
 		System.out.println("TPS: " + ((float)(threadNum*TPPTHREAD)/(runSqlTotalTime))*1000+"/s" );
 	}
 }
